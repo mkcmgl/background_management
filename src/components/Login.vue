892cc9089292cf860a1540dc2be05ca252ecffb7
mkcmgl <2515318247@qq.com>
2022-11-15 19:05:03 +0800

    home-save-token-session storage

---
+++ src/components/Login.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="login">
+        <div class="center">
+            <el-button type="primary" @click="setToken">登录</el-button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'login',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        setToken(){
+            const myToken ={
+                token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjUwMCwicmlkIjowLCJpYXQiOjE1MTI1NDQyOTksImV4cCI6MTUxMjYzMDY5OX0.eGrsrvwHm-tPsO9r_pxHIQ5i5L1kX9RX444uwnRGaIM"
+            }
+            window.sessionStorage.setItem("token",myToken.token)
+            this.$router.push('/home')
+        }
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.login{
+    height: 100%;
+    background-color: #809dbb;
+}
+.center{
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    margin: auto;
+    width: 30%;
+    height: 50%;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    component: Login
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/login',
+    name: 'login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../components/Login.vue')
+  },
+  {
+    path: '/home',
+    name: 'home',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component:Home
   }
 ]
 
