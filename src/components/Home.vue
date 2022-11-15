b9dcded632d105c938a2641d77398c2a1edd4fdd
mkcmgl <2515318247@qq.com>
2022-11-15 22:48:04 +0800

    menu

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,39 @@
 <template>
-    <div>
-        home
-        <el-button type="info" @click="logout">退出</el-button>
-    </div>
+
+    <el-container class="home-container">
+        <el-header>
+            <div>
+                <img src="../assets/heima.png" alt="?">
+                <span>电商管理平台</span>
+            </div>
+            <el-button type="info" @click="logout">退出</el-button>
+
+        </el-header>
+        <el-container>
+            <el-aside width="200px">
+                <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
+                    background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+                    <el-submenu index="1">
+                        <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span>导航一</span>
+                        </template>
+
+                        <el-menu-item index="1-4-1"> 
+                            <template slot="title">
+                                <i class="el-icon-location"></i>
+                                <span>导航一</span>
+                            </template>
+                        </el-menu-item>
+
+                    </el-submenu>
+
+                </el-menu>
+            </el-aside>
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
+
 </template>
 
 <script>
@@ -11,23 +42,68 @@ export default {
 
     data() {
         return {
-            
+
         };
     },
-
+    created() {
+        this.getMenuList()
+    },
     mounted() {
-        
+
     },
 
     methods: {
-        logout(){
-            window.sessionStorage.removeItem('token')
+        logout() {
+            window.sessionStorage.removeItem('token');
             this.$router.replace('/login')
-        } 
+        },
+        handleClose() {
+
+        },
+        handleOpen() { },
+        getMenuList(){
+          const res=  this.$http.get('menus')
+          console.log(res)
+        }
     },
 };
 </script>
 
 <style lang="scss" scoped>
+.el-header {
+    background-color: #373d41;
+    color: #FFFF;
+    font-size: 1.25rem;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
 
+    >div {
+        display: flex;
+        align-items: center;
+
+        span {
+            margin-left: .9375rem;
+        }
+    }
+}
+
+.el-aside {
+    background-color: #333744;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+}
+
+.el-main {
+    background-color: #FFFF;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+}
+
+.home-container {
+    height: 100%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,13 @@ import store from './store'
 import './plugins/element.js'
 import '@/assets/css/global.css'
 Vue.config.productionTip = false
-
+import axios from 'axios';
+axios.defaults.baseURL='http://121.4.255.161:8888/api/private/v1/'
+axios.interceptors.request.use(config=>{
+  config.headers.Authorization=window.sessionStorage.getItem('token')
+  return config
+})
+Vue.prototype.$http=axios
 new Vue({
   router,
   store,
